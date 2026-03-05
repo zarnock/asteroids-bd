2043b29d4a25251141797b46b97cebf227c5dbcc
Zarnock <zarnock1212@gmail.com>
2026-03-04 23:01:49 -0500

    Updated game loop to use sprite groups

--- main.py
+++ main.py
@@ -7,12 +7,32 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    # Game loop 
+    # Game loop (new)
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        updatable.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+'''    # Game loop 
     while True:
         log_state()
         for event in pygame.event.get():
@@ -22,7 +42,7 @@ def main():
         player.draw(screen)
         player.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000'''
 
 
 if __name__ == "__main__":
