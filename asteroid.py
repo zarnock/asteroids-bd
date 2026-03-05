b930be846345280d4ab4961f46365894c35a0a0a
Zarnock <zarnock1212@gmail.com>
2026-03-05 00:46:23 -0500

    Added collision, removed old game loop

--- asteroid.py
+++ asteroid.py
@@ -5,7 +5,7 @@ import pygame
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.position = (x, y)
+        self.position = pygame.Vector2(x, y)
         self.radius = radius
 
     def draw(self, screen):
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import *
 from asteroid import *
 from asteroidfield import AsteroidField
@@ -32,24 +33,17 @@ def main():
         screen.fill("black")
         updatable.update(dt)
 
+        for a in asteroids:
+            if a.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         for d in drawable:
             d.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-'''    # Game loop 
-    while True:
-        log_state()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000'''
-
 
 if __name__ == "__main__":
     main()
